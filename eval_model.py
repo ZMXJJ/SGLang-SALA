82343705715777421896a3b86370d46cdea7778b
Dennis <huangsiyuan@modelbest.cn>
2026-02-25 19:54:36 +0800

    fix acc metrics

--- eval_model.py
+++ eval_model.py
@@ -431,7 +431,8 @@ def main():
             "task_id": task_id,
             "record_id": record_id,
             "user_id": user_id,
-            "overall_accuracy": avg_score,
+            "ori_accuracy": round(avg_score, 2),
+            "overall_accuracy": round(avg_score / 81 * 100, 2) ,
             "duration": duration,
             "total_tokens": total_output_tokens
         }, f, ensure_ascii=False, indent=2)
